dafd03566e87147671e0f58e1303da14471e4c03
amansgz <amansgz.ar@gmail.com>
2023-09-05 16:46:05 -0300

    Update styles and scripts

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,16 @@
+/*Clicked button styling and showing menu social links to share article*/
+const cardFooter = document.querySelector('.card__footer');
+const articleDetails = document.querySelector('.article__details');
+const socialLinks = document.querySelector('.social__links');
+const iconShareArticle = document.querySelector('.icon__share__article');
+const svgArrowShare = document.querySelector('.svg__share');
 
-const cardFooter = document.querySelector('.card-footer');
-const avatar = document.querySelector('.writer-avatar');
-const name = document.querySelector('.writer-name');
-const date = document.querySelector('.writer-date');
-const iconShare = document.querySelector('.icon-share');
-const socialLinks = document.querySelector('.social-links');
+iconShareArticle.addEventListener('click', () => {
+	iconShareArticle.classList.toggle('icon__share__clicked');
+	svgArrowShare.classList.toggle('icon__share__clicked');
 
-iconShare.addEventListener('click', () => {
-	cardFooter.classList.toggle('bg-active-footer');
-	avatar.classList.toggle('hide');
-	name.classList.toggle('hide');
-	date.classList.toggle('hide');
+	cardFooter.classList.toggle('bg-dark');
+	articleDetails.classList.toggle('hide');
 	socialLinks.classList.toggle('show');
 })
 
